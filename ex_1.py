f77a86df5dae483abe4172e85d9321128aedaf5b
EnricoHuber <enricohuber5@gmail>
2021-09-06 23:57:48 +0200

    Exercise 1

---
+++ ex_1.py
@@ -0,0 +1,21 @@
+import random
+
+
+def guessing_game():
+    guessed = False
+    num = random.randint(0, 100)
+    while not guessed:
+        try:
+            attempt = int(input("Try to guess the number! Type an integer -------> "))
+        except:
+            print("Wrong input, try again. Type an integer -------> ")
+        if attempt == num:
+            print(f"Yeah you got it! The number was {num}. Congratulations!\n")
+            guessed = True
+        elif attempt < num:
+            print(f"{attempt} is too low, try again!\n")
+        elif attempt > num:
+            print(f"{attempt} is too high, try again!\n")
+
+
+guessing_game()
